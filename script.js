570b8a5ee01173b82863b1531688731f7c59bf10
Zhyldyz <jargynbaeva@gmail.com>
2022-01-12 12:51:12 +0200

    part1 added

---
+++ script.js
@@ -0,0 +1,55 @@
+const carousel = document.querySelector('.carousel');
+let sliders = [];
+
+let slideIndex = 0;
+
+const createSlide = () => {
+    if(slideIndex === movies.length){
+        // console.log(slideIndex === movies.length, 'true')
+        slideIndex = 0;
+    }
+
+
+// creating DOM elements
+
+    let slide = document.createElement('div');
+    let content = document.createElement('div');
+    let imgElement = document.createElement('img');
+    let h1 = document.createElement('h1');
+    let p = document.createElement('p');
+
+//attaching all elements
+     imgElement.appendChild(document.createTextNode(''))
+     h1.appendChild(document.createTextNode(movies[slideIndex].name));
+     p.appendChild(document.createTextNode(movies[slideIndex].des));
+     content.appendChild(h1);
+     content.appendChild(p);
+     slide.appendChild(content);
+     slide.appendChild(imgElement);
+     carousel.appendChild(slide);
+   
+
+ // setting up img
+     imgElement.src = movies[slideIndex].image;
+     slideIndex++;
+
+ //setting elements classname
+    slide.className = 'slider';
+    content.className = 'slide-content';
+    h1.className = 'movie-title';
+    p.className = 'movie-des';
+
+    sliders.push(slide)
+  
+
+    // adding slider effect
+    if(sliders.length){
+        sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${30 *(sliders.length -2)}px)`
+    }
+}
+for(let i=0; i< 3; i++){
+    createSlide()
+}
+// setInterval(() => {
+//     createSlide()
+// }, 3000)
